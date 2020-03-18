11b6a4b4caf8b1dc6f75970c6c19ae53ee6c6073
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-17 23:49:30 -0600

    Added a project on Abstract classes

---
+++ AbstractClasses2/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClasses2
+{
+    public abstract class Customer
+    {
+        public abstract void Print();
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello from the inherited implemented method!!!");
+        }
+    }
+}
---
+++ AbstractClasses2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractClasses2
+{
+    public class Program : Customer
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+
+            // Using the method that was forced to be implemented:
+            p.Print();
+
+            // Using an implemented method in the abstract class, which was inherited by Program:
+            p.Greet();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Hello from the recently implemented Print() method!");
+        }
+    }
+}
